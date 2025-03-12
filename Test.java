869dc6accb8be68090cb38e178977fc48feb702b
Kiran-mane24 <166535405+Kiran-mane24@users.noreply.github.com>
2025-03-12 15:24:31 +0530

    Update Test.java Kiran 12 march

--- Test.java
+++ Test.java
@@ -3,8 +3,6 @@ class Test
 public static void main(String ar[])
 {
 System.out.println("hello");
-System.out.println("welcome to Java and Git Jenkins Demo");
-System.out.println("asAD");
-System.out.println("Automation with Webhook");
+System.out.println("WebHook Demo");
 }
 }
